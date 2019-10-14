0c595a6246154e3cddd60d449964d0e37c96d9a1
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-13 19:49:48 -0600

    Third commit

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,6 +16,16 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    ans = []
+
+    @board.each do |row|
+      @row.each do |col|
+        if @board[row][col].empty?
+          new_board = @board.dup
+          
+        end
+      end
+    end
   end
 end
 
